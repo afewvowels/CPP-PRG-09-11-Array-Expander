9aec4539355725fe5e43a557826d0b2f8755a689
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-31 20:09:23 -0400

    Wrong prompt written.

--- CPP-PRG-09-11-Array-Expander/main.cpp
+++ CPP-PRG-09-11-Array-Expander/main.cpp
@@ -5,11 +5,17 @@
 //  Created by Keith Smith on 10/31/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a function that accepts an int array and the array's size as arguments. The
+//  function should create a new array that is twice the size of the argument array. The
+//  function should copy the contents of the argument array to the new array and intialize
+//  the unused elements of the second array with 0. The function should return a pointer
+//  to the new array.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    
+    
     return 0;
 }
