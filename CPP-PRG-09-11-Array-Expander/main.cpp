31175306472d71ad5805e55864b3b46300719bfc
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-31 20:24:57 -0400

    Needs comments.

--- CPP-PRG-09-11-Array-Expander/main.cpp
+++ CPP-PRG-09-11-Array-Expander/main.cpp
@@ -13,9 +13,88 @@
 
 #include <iostream>
 
+using namespace std;
+
+int *getArraySize();
+int *fillArray(int *, int);
+void modifyArray(int *, int);
+
 int main()
 {
+    int *intArrSize = nullptr;
+    int *intArrNumbers = nullptr;
     
+    intArrSize = getArraySize();
+    
+    intArrNumbers = fillArray(intArrNumbers, *intArrSize);
+    
+    modifyArray(intArrNumbers, *intArrSize);
     
     return 0;
+}
+
+int *getArraySize()
+{
+    int *intArrSize = nullptr;
+    
+    intArrSize = new int;
+    
+    cout << "Please enter the size of the initial array: ";
+    cin >> *intArrSize;
+    while(!cin || *intArrSize <= 1 || *intArrSize > 100)
+    {
+        cout << "Please enter a number between 2 and 100: ";
+        cin.clear();
+        cin.ignore();
+        cin >> *intArrSize;
+    }
+    
+    return intArrSize;
+}
+
+int *fillArray(int *intArr, int intSize)
+{
+    intArr = new int[intSize];
+    
+    for (int i = 0 ; i < intSize ; i++)
+    {
+        cout << "Please enter number " << (i + 1) << " of " << intSize << ": ";
+        cin >> intArr[i];
+        while(!cin || intArr[i] < -100 || intArr[i] > 100)
+        {
+            cout << "Enter a number between -100 and 100: ";
+            cin.clear();
+            cin.ignore();
+            cin >> intArr[i];
+        }
+    }
+    
+    return intArr;
+}
+
+void modifyArray(int *intArr, int intSize)
+{
+    int *intSizeNew = nullptr;
+    int *intArrNew = nullptr;
+    
+    intSizeNew = new int;
+    
+    *intSizeNew = 2 * intSize;
+    
+    intArrNew = new int[*intSizeNew];
+    
+    for (int i = 0 ; i < intSize ; i++)
+    {
+        intArrNew[i] = intArr[i];
+    }
+    
+    for (int i = intSize ; i < *intSizeNew ; i++)
+    {
+        intArrNew[i] = 0;
+    }
+    
+    for ( int i = 0 ; i < *intSizeNew ; i++)
+    {
+        cout << intArrNew[i] << endl;
+    }
 }
